740fe5461d55aa3cbffc037d7f2dae90550498a5
Star Wu <srw9548@nyu.edu>
2025-04-29 18:57:27 +0800

    prototype B

---
+++ ProjectBPrototype copy/sketch.js
@@ -0,0 +1,192 @@
+let stage = 0;
+let question1;
+let answerColor;
+
+let topFlowerImg1;
+let imageTop;
+let imageMid;
+let imageBot;
+
+
+let flowers = [];
+
+
+function preload(){
+
+  botFlower1 = loadImage("assets/botFlower1.png");
+  botFlower2 = loadImage("assets/botFlower2.png");
+  botFlower3 = loadImage("assets/botFlower3.webp");
+  botFlower4 = loadImage("assets/botflower4.webp");
+
+  midFlower1 = loadImage("assets/midFlower1.png");
+  midFlower2 = loadImage("assets/midFlower2.png");
+  midFlower3 = loadImage("assets/midFlower3.png");
+  midFlower4 = loadImage("assets/midFlower4.png");
+
+  topFlower1 = loadImage("assets/topFlower1.gif");
+  topFlower2 = loadImage("assets/topFlower2.webp");
+  topFlower3 = loadImage("assets/topFlower3.png");
+  topFlower4 = loadImage("assets/topFlower4.webp");
+}
+
+function setup() {
+  createCanvas(800, 500);
+  rectMode(CENTER);
+  
+}
+
+function draw() {
+  background(0);
+  
+  if (stage == 0) {
+    stage0();
+    // for(let i = 0; i < flowers.length; i ++) {
+    //   flowers[i].display(x, y);
+    // }
+  } else if (stage == 1) {
+    stage1();
+  } else if (stage == 2) {
+    stage2();
+  } else if (stage == 3) {
+    stage3();
+  } else if (stage == 'displayFlower') {
+    // displayFlower();
+    flowers.push(new Flower(imageTop, imageMid, imageBot))
+    flowers[flowers.length - 1].display(width / 2, height / 2);
+    drawButton();
+  } 
+}
+
+function mouseClicked() {
+  // change stage based on clicking
+  if(stage == 0) {
+    let mouseDistStart = dist(mouseX, mouseY, width/2, height * 3/4);
+    if (mouseDistStart <= 50) {
+      stage = 1;
+    }
+  } else if (stage == 1) {
+    let mouseDist1 = dist(mouseX, mouseY, width / 4, height / 2);
+    let mouseDist2 = dist(mouseX, mouseY, width * 3 / 4, height / 2);
+    let mouseDist3 = dist(mouseX, mouseY, width / 4, height / 2);
+    let mouseDist4 = dist(mouseX, mouseY, width * 3 / 4, height / 2);
+    if (mouseDist1 <= 25) {
+      imageBot = botFlower1
+      stage = 2;
+    } else if (mouseDist2 <= 25) {
+      imageBot = botFlower2
+      stage = 2;
+    }
+    // }else if (mouseDist3 <= 25) {
+    //   imageBot = 
+    //   stage = 2;
+    // }else if (mouseDist4 <= 25) {
+    //   imageBot = 
+    //   stage = 2;
+    // }
+  } else if (stage == 2) {
+    let mouseDist1 = dist(mouseX, mouseY, width / 4, height / 2);
+    let mouseDist2 = dist(mouseX, mouseY, width * 3 / 4, height / 2);
+    if (mouseDist1 <= 25) {
+      imageMid = midFlower1
+      stage = 3;
+    } else if (mouseDist2 <= 25) {
+      imageMid = midFlower2
+      stage = 3;
+    }
+  } else if (stage == 3) {
+    let mouseDist1 = dist(mouseX, mouseY, width / 4, height / 2);
+    let mouseDist2 = dist(mouseX, mouseY, width * 3 / 4, height / 2);
+    if (mouseDist1 <= 25) {
+      imageTop = topFlower1
+      stage = 'displayFlower';
+    } else if (mouseDist2 <= 25) {
+      imageTop = topFlower2
+      stage = 'displayFlower';
+    }
+  } else if (stage == 'displayFlower') {
+    let mouseDistDone = dist(mouseX, mouseY, width/2, height * 3.5/4);
+    if (mouseDistDone <= 50) {
+      stage = 0;
+   }
+  }
+}
+
+function stage0() {
+  rect(width/2, height * 3/4, 100, 50, 10);
+}
+
+function stage1() {
+  circle(width / 4, height / 2, 50);
+  circle(width * 3 / 4, height / 2, 50);
+  circle(width * 3 / 4, height / 2, 50);
+  circle(width * 3 / 4, height / 2, 50);
+
+}
+
+function stage2() {
+  rect(width / 4, height / 2, 50);
+  rect(width * 3 / 4, height / 2, 50);
+}
+
+function stage3() {
+  circle(width / 4, height / 2, 50);
+  circle(width * 3 / 4, height / 2, 50);
+}
+
+class Flower {
+  constructor(imgTop, imgMid, imgBot) {
+    this.imgTop = imgTop;
+    this.imgMid = imgMid;
+    this.imgBot = imgBot;
+    this.x = 250;
+    this.y = 50;
+  }
+
+  display() {
+    // console.log(imageTop)
+    push();
+    translate(this.x, this.y);
+
+    //flowerBot
+    push();
+    scale(0.16);
+    image (imageBot, 100, 1200);
+    pop();
+
+    //flowerMid
+    push();
+    scale(0.16);
+    image (imageMid, 400, 500);
+    pop();
+    
+    //flowerTop
+    push();
+    scale(0.16);
+    image (imageTop, 0, 0);
+    pop();
+
+    pop();
+    
+  }
+}
+
+function drawButton(){
+  //doneButton
+  push();
+  translate(width/2, height * 3.5/4);
+  fill(255);
+  rect(0, 0, 100, 50, 10);
+  //text
+  fill(0);
+  textSize(20);
+  text("Done", -22, 8);
+  pop();
+  
+}
+
+
+// let f = new Flower();
+
+
+// f.head = 2
+// f.stem = 3
--- ProjectBPrototype/sketch.js
+++ ProjectBPrototype/sketch.js
@@ -2,18 +2,29 @@ let stage = 0;
 let question1;
 let answerColor;
 
+let topFlowerImg1;
+let image1;
+// let topChoice, midChoice, botChoice;
+
 
 // function preload() {
 //   image1 = 'filepath'
 //   image2 = 'filepath'
 // }
+
+function preload(){
+  topFlowerImg1 = loadImage("assets/topFlowerGif.gif");
+  Flower2 = loadImage("assets/Flower2.webp");
+}
+
 function setup() {
   createCanvas(800, 500);
   rectMode(CENTER);
+  
 }
 
 function draw() {
-  background(10);
+  background(0);
   
   if (stage == 0) {
     stage0();
@@ -25,49 +36,53 @@ function draw() {
     stage3();
   } else if (stage == 'displayFlower') {
     displayFlower()
+    //flower = new Flower(topChoice, midChoice, botChoice);
   }
 }
 
 function mouseClicked() {
   // change stage based on clicking
   if(stage == 0) {
-    let mouseDist = dist(mouseX, mouseY, width/2, height * 3/4);
-    if (mouseDist <= 50) {
+    let mouseDistStart = dist(mouseX, mouseY, width/2, height * 3/4);
+    if (mouseDistStart <= 50) {
       stage = 1;
     }
   } else if (stage == 1) {
-    let mouseDistLeft = dist(mouseX, mouseY, width / 4, height / 2);
-    let mouseDistRight = dist(mouseX, mouseY, width * 3 / 4, height / 2);
-    if (mouseDistLeft <= 25) {
+    let mouseDist1 = dist(mouseX, mouseY, width / 4, height / 2);
+    let mouseDist2 = dist(mouseX, mouseY, width * 3 / 4, height / 2);
+    if (mouseDist1 <= 25) {
       answerColor = color(255, 0, 0);
+      
       stage = 2;
-    } else if (mouseDistRight <= 25) {
+    } else if (mouseDist2 <= 25) {
       answerColor = color(0, 255, 0);
       stage = 2;
     }
   } else if (stage == 2) {
-    let mouseDistLeft = dist(mouseX, mouseY, width / 4, height / 2);
-    let mouseDistRight = dist(mouseX, mouseY, width * 3 / 4, height / 2);
-    if (mouseDistLeft <= 25) {
+    let mouseDist1 = dist(mouseX, mouseY, width / 4, height / 2);
+    let mouseDist2 = dist(mouseX, mouseY, width * 3 / 4, height / 2);
+    if (mouseDist1 <= 25) {
       answerColor2 = color(255, 0, 0);
       stage = 3;
-    } else if (mouseDistRight <= 25) {
+    } else if (mouseDist2 <= 25) {
       answerColor2 = color(0, 0, 255);
       stage = 3;
     }
   } else if (stage == 3) {
-    let mouseDistLeft = dist(mouseX, mouseY, width / 4, height / 2);
-    let mouseDistRight = dist(mouseX, mouseY, width * 3 / 4, height / 2);
-    if (mouseDistLeft <= 25) {
-      answerColor3 = color(0, 255, 0);
+    let mouseDist1 = dist(mouseX, mouseY, width / 4, height / 2);
+    let mouseDist2 = dist(mouseX, mouseY, width * 3 / 4, height / 2);
+    if (mouseDist1 <= 25) {
+      // image1 = topFlowerImg1
+      // topChoice = 0;
       stage = 'displayFlower';
-    } else if (mouseDistRight <= 25) {
-      answerColor3 = color(255, 255, 0);
+    } else if (mouseDist2 <= 25) {
+      // image1 = Flower2
+      // topChoice = 1
       stage = 'displayFlower';
     }
   } else if (stage == 'displayFlower') {
-    let mouseDist = dist(mouseX, mouseY, width/2, height * 3.5/4);
-    if (mouseDist <= 50) {
+    let mouseDistDone = dist(mouseX, mouseY, width/2, height * 3.5/4);
+    if (mouseDistDone <= 50) {
       stage = 0;
    }
   }
@@ -104,8 +119,8 @@ function displayFlower() {
   fill(answerColor2)
   rect(width / 2, 200, 100);
   //flowerTop
-  fill(answerColor3)
-  rect(width / 2, 100, 100);
+  scale(0.16);
+  image (image1, 1600, 150);
   pop();
 
   //doneButton
@@ -120,3 +135,48 @@ function displayFlower() {
   pop();
   
 }
+
+
+// let f = new Flower();
+
+
+// f.head = 2
+// f.stem = 3
+
+
+
+
+/*
+class Flower
+  constructor(imgIndex)
+    this.head = 0 //0, 1, 2, 3 -> imgIndex
+    this.stem = 0 //0, 2, 3,3 , 4
+    this.leaf = 0
+    this.age
+
+
+    display
+      push 
+      translate()
+      scale(1)
+
+      if(this.head == 0)
+        push
+        translate
+        scale() // 
+        flower nr 1
+
+
+        pop
+      else if
+         ...
+
+
+
+
+
+         fill("red")
+         circle(0, 0, 5)
+      pop
+
+*/
